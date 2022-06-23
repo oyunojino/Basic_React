d5bf72a3f6443afbfa5c179a5943a2dfef15d9fd
ohyunjin <yjoh@kci.co.kr>
2022-06-23 09:40:34 +0900

    컴포넌트를 불러와서 사용

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Hello from './Hello';
+//                '.Hello(.js)'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload.*/}
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
     </div>
   );
 }
