801adaf5b2c3f86e09c178b93b1f185ca4d13b26
ohyunjin <yjoh@kci.co.kr>
2022-06-23 09:39:58 +0900

    Hello 컴포넌트 생성

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,3 +1,11 @@
 // 리액트를 불러와서 사용하겠다
 import React from 'react';
 
+// 컴포넌트 이름은 대분자로 시작
+function Hello() {
+    // html 같지만 'JSX' 라고 부름
+    return <div>Hello</div>
+}
+
+// Hello 라는 컴포넌트를 만들어서 내보내주겠다
+export default Hello;
